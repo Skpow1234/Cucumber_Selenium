ab1525cc50c1f0e29495052669c0c2c479e34af3
Juan Hurtado <jfhvjfhv0015@gmail.com>
2021-11-24 04:40:08 -0500

    Agregamos SignIn

--- src/test/java/features/test.feature
+++ src/test/java/features/test.feature
@@ -1,12 +1,25 @@
 Feature: Registrarse en efset.org
-  Como usuario quiero registrarme en la pagina de efset
+  Como usuario quiero registrarme y logearme en la pagina de efset
 
   Background:
     Given estoy en la página principal de efset.org
     And tengo internet
     And quiero crear una cuenta en "efset.org"
 
-  Scenario: Registrar una cuenta en "efset.org"
+
+  Scenario: Crear una cuenta en "efset.org"
+    Given estoy en la página principal de efset.org
+    When doy click en el botón de "Login"
+    When doy click en el boton de "Create one now"
+    When doy click en el boton de "SIGN UP WITH EMAIL"
+    And ingreso mi primer nombre en la barra de texto de "First Name"
+    And ingreso mi primer nombre en la barra de texto de "Last Name"
+    And ingreso un email en la barra de texto de "Email"
+    And ingreso una contraseña en la barra de "Password"
+    And ingreso nuevamenta la contraseña en la barra de "Password Confirmation"
+    When doy click en el boton "SIGN UP"
+
+  Scenario: Logear una cuenta en "efset.org"
     Given estoy en la página principal de efset.org
     When doy click en el botón de "Login"
     And soy redireccionado a una página donde tengo que ingresar unos datos
@@ -15,13 +28,3 @@ Feature: Registrarse en efset.org
     When doy click en el boton de "SIGN IN"
     And soy redireccionado a la pagina principal de efset.org
     Then en donde previamente estaba el boton de "Login" aparece un avatar de una persona con un menu desplegable
-
-  Scenario: Cancelar suscripcion de email
-    Given estoy en la página principal de efset.org
-    When doy click en el boton del footer "Cookies"
-    And soy redireccionado a cookie-policy
-    When doy click en el boton de "Cancelar Suscripcion"
-    And ingreso mi email en la barra de texto para "Email"
-    When doy click en el boton de "recaptcha-button"
-    When doy click en el boton de "Enviar"
-    And soy redireccionado a la pagina de "unsuscribe"
--- src/test/java/seleniumgluecode/Test.java
+++ src/test/java/seleniumgluecode/Test.java
@@ -9,12 +9,15 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.concurrent.TimeUnit;
+
+@SuppressWarnings("ALL")
 public class Test {
 
     private ChromeDriver driver;
 
     @Given("^estoy en la página principal de efset.org$")
-    public void estoy_en_la_página_principal_de_efset_org() throws Throwable {
+    public void estoy_en_la_página_principal_de_efset_org() {
         System.setProperty("webdriver.chrome.driver","./src/test/resources/chromedriver/chromedriver.exe");
         /*
             Creamos objeto ChromeDriver,
@@ -27,74 +30,94 @@ public class Test {
     }
 
     @Given("^tengo internet$")
-    public void tengo_internet() throws Throwable {
+    public void tengo_internet() {
 
     }
 
     @Given("^quiero crear una cuenta en \"([^\"]*)\"$")
-    public void quiero_crear_una_cuenta_en(String arg1) throws Throwable {
+    public void quiero_crear_una_cuenta_en(String arg1) {
 
     }
 
     @When("^doy click en el botón de \"([^\"]*)\"$")
-    public void doy_click_en_el_botón_de(String arg1) throws Throwable {
+    public void doy_click_en_el_botón_de(String arg1) {
         WebElement loginLocator = driver.findElement(By.className("jss20"));
         loginLocator.click();
     }
 
     @When("^soy redireccionado a una página donde tengo que ingresar unos datos$")
-    public void soy_redireccionado_a_una_página_donde_tengo_que_ingresar_unos_datos() throws Throwable {
+    public void soy_redireccionado_a_una_página_donde_tengo_que_ingresar_unos_datos() {
     }
 
     @When("^ingreso un email en la barra de texto de \"([^\"]*)\"$")
-    public void ingreso_un_email_en_la_barra_de_texto_de(String arg1) throws Throwable {
+    public void ingreso_un_email_en_la_barra_de_texto_de(String arg1) {
         driver.findElement(By.id("email"));
         WebElement username=driver.findElement(By.id("email"));
         username.sendKeys("jfhvjfhv0015@gmail.com");
     }
     @When("^ingreso una contraseña en la barra de \"([^\"]*)\"$")
-    public void ingreso_una_contraseña_en_la_barra_de(String arg1) throws Throwable {
+    public void ingreso_una_contraseña_en_la_barra_de(String arg1) {
         driver.findElement(By.id("password"));
         WebElement password= driver.findElement(By.id("password"));
         password.sendKeys("Skpow1234");
     }
 
     @When("^doy click en el boton de \"([^\"]*)\"$")
-    public void doy_click_en_el_boton_de(String arg1) throws Throwable {
-        WebElement signInLocator =driver.findElement(By.id("submit-form-btn"));
+    public void doy_click_en_el_boton_de(String arg1) {
+        WebElement signInLocator = driver.findElement(By.id("submit-form-btn"));
         signInLocator.click();
     }
 
-    @When("^soy redireccionado a la pagina principal de efset\\.org$")
-    public void soy_redireccionado_a_la_pagina_principal_de_efset_org() throws Throwable {
+    @When("^soy redireccionado a la pagina principal de efset.org$")
+    public void soy_redireccionado_a_la_pagina_principal_de_efset_org() {
     }
 
     @Then("^en donde previamente estaba el boton de \"([^\"]*)\" aparece un avatar de una persona con un menu desplegable$")
-    public void en_donde_previamente_estaba_el_boton_de_aparece_un_avatar_de_una_persona_con_un_menu_desplegable(String arg1) throws Throwable {
-       String actualUrl ="https://accounts.ef.com/en/efset/login/index.html?continue_uri=https%3A%2F%2Faccounts.ef.com%2Foauth2%2Fauth%3Fresponse_type%3Dtoken%26client_id%3Defcom_efset%26scope%3D%26providers%3Dfacebook%252Cgoogle%252Clinkedin%26redirect_uri%3Dhttps%253A%252F%252Fwww.efset.org%252Fredirect%252F%26state%3Dhttp%253A%252F%252Fwww.efset.org%252F";
-       String expectedUrl = driver.getCurrentUrl();
+    public void en_donde_previamente_estaba_el_boton_de_aparece_un_avatar_de_una_persona_con_un_menu_desplegable(String arg1) {
+        String actualUrl ="https://accounts.ef.com/en/efset/login/index.html?continue_uri=https%3A%2F%2Faccounts.ef.com%2Foauth2%2Fauth%3Fresponse_type%3Dtoken%26client_id%3Defcom_efset%26scope%3D%26providers%3Dfacebook%252Cgoogle%252Clinkedin%26redirect_uri%3Dhttps%253A%252F%252Fwww.efset.org%252Fredirect%252F%26state%3Dhttp%253A%252F%252Fwww.efset.org%252F";
+        String expectedUrl = driver.getCurrentUrl();
         Assert.assertEquals(actualUrl, expectedUrl);
     }
-    @When("^doy click en el boton del footer \"([^\"]*)\"$")
-    public void doy_click_en_el_boton_del_footer(String arg1) throws Throwable {
-        WebElement cookiesLocator = driver.findElement(By.className("MuiTypography-root MuiTypography-caption"));
-        cookiesLocator.click();
+    @When("^ingreso mi primer nombre en la barra de texto de \"([^\"]*)\"$")
+    public void ingreso_mi_primer_nombre_en_la_barra_de_texto_de(String arg1) throws Throwable {
+        driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+        WebElement signInLocator = driver.findElement(By.id("submit-form-btn"));
+        signInLocator.click();
 
     }
 
-    @When("^soy redireccionado a cookie-policy$")
-    public void soy_redireccionado_a_cookie_policy() throws Throwable {
-
+    @When("^ingreso nuevamenta la contraseña en la barra de \"([^\"]*)\"$")
+    public void ingreso_nuevamenta_la_contraseña_en_la_barra_de(String arg1) throws Throwable {
+        driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+        WebElement createOneLocator = driver.findElement(By.id("link-to-register"));
+        createOneLocator.click();
+        driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+        WebElement signUpEmailLocator = driver.findElement(By.id("sign-up-open"));
+        signUpEmailLocator.click();
+        driver.findElement(By.id("first"));
+        WebElement name=driver.findElement(By.id("first"));
+        name.sendKeys("Juan Felipe");
+        driver.findElement(By.id("last"));
+        WebElement lastname=driver.findElement(By.id("last"));
+        lastname.sendKeys("Hurtado Villani");
+        driver.findElement(By.id("email"));
+        WebElement username=driver.findElement(By.id("email"));
+        username.sendKeys("thenewgodsk1@gmail.com");//Cambiar correo cada que se corre una prueba
+        driver.findElement(By.id("password"));
+        WebElement password= driver.findElement(By.id("password"));
+        password.sendKeys("Skpow1234");
+        driver.findElement(By.id("confirmation"));
+        WebElement passwordConf= driver.findElement(By.id("confirmation"));
+        passwordConf.sendKeys("Skpow1234");
     }
 
-    @When("^ingreso mi email en la barra de texto para \"([^\"]*)\"$")
-    public void ingreso_mi_email_en_la_barra_de_texto_para(String arg1) throws Throwable {
-
+    @When("^doy click en el boton \"([^\"]*)\"$")
+    public void doy_click_en_el_boton(String arg1) throws Throwable {
+        WebElement signUpBtnLocator = driver.findElement(By.id("submit-form-btn"));
+        signUpBtnLocator.click();
     }
 
-    @When("^soy redireccionado a la pagina de \"([^\"]*)\"$")
-    public void soy_redireccionado_a_la_pagina_de(String arg1) throws Throwable {
 
-    }
+
 
 }
